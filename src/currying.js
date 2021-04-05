f5b6bc960c695edd31889c17d390c0ea04e40e0c
Roman Menderov <vladimirovich3112@mail.ru>
2021-04-05 20:00:16 +0300

    add curring test

--- src/currying.js
+++ src/currying.js
@@ -3,15 +3,16 @@
 // Пример использования функции
 
 
-function func (a:number, b:number,
-   c:number, d:number, e:number){return a + b + c + d + e}
+function func (a, b,
+   c, d, e){return a + b + c + d + e}
 
 
-function yourFunction(myFunc:func){
+// eslint-disable-next-line @typescript-eslint/ban-types
+function yourFunction(myFunc){
 
     return function curried(...args) {
       if (args.length >= myFunc.length) {
-        return myFunc.apply(this, args);
+        return myFunc.apply(this , args);
       }
         return function result(...args2) {
           return curried.apply(this, args.concat(args2));
@@ -22,9 +23,3 @@ function yourFunction(myFunc:func){
   }
 
 export const hof = yourFunction(func);
-
-console.log(hof(1, 2, 3, 4, 5)); // 15
-console.log(hof(2, 3, 4)(5, 6)); // 20
-console.log(hof(3, 4)(5, 6)(7)); // 25
-console.log(hof(4, 5)(6)(7, 8)); // 30
-console.log(hof(5)(6)(7)(8)(9)); // 35
---
+++ src/currying.test.ts
@@ -0,0 +1,37 @@
+/* eslint-disable no-console */
+import {hof} from './currying'
+
+describe("it should return correct value", () => {
+  const originalConsoleLog = console.log;
+
+  beforeEach(() => {
+    console.log = jest.fn();
+  });
+  afterEach(() => {
+    jest.resetAllMocks();
+    console.log = originalConsoleLog;
+  });
+
+  it("should return 15", () => {
+    console.log(hof(1, 2, 3, 4, 5));
+    expect(console.log).toHaveBeenCalledWith(15);
+  });
+  
+  it("should return 20", () => {
+    console.log(hof(2, 3, 4)(5, 6));
+    expect(console.log).toHaveBeenCalledWith(20);
+  });
+  it("should return 25", () => {
+    console.log(hof(3, 4)(5, 6)(7));
+    expect(console.log).toHaveBeenCalledWith(25);
+  });
+  it("should return 30", () => {
+    console.log(hof(4, 5)(6)(7, 8)); // 30
+    expect(console.log).toHaveBeenCalledWith(30);
+  });
+  it("should return 35", () => {
+    console.log(hof(5)(6)(7)(8)(9)); // 35
+    expect(console.log).toHaveBeenCalledWith(35);
+  });
+ 
+});
